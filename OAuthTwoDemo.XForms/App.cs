5c66b9c3a4a44dcf5b3d08bd668ea50eb8cbfdc6
Joe Sauve <joe@joesauve.com>
2014-07-31 09:48:01 -0500

    Just some updates to the comments

--- OAuthTwoDemo.XForms/App.cs
+++ OAuthTwoDemo.XForms/App.cs
@@ -20,10 +20,13 @@ namespace OAuthTwoDemo.XForms
 							_Instance = new App ();
 							_Instance.OAuthSettings = 
 								new OAuthSettings (
-									clientId: "3b523a97d57e428488da56ad80789119",  // your OAuth2 client id 
-									scope: "basic",  // The scopes for the particular API you're accessing. The format o this will vary by API.
-									authorizeUrl: "https://instagram.com/oauth/authorize",  // the auth URL for the service
-									redirectUrl: "http://www.joesauve.com/using-xamarin-auth-with-xamarin-forms/"); // the redirect URL for the service
+									clientId: "",  		// your OAuth2 client id 
+									scope: "",  		// The scopes for the particular API you're accessing. The format for this will vary by API.
+									authorizeUrl: "",  	// the auth URL for the service
+									redirectUrl: "");   // the redirect URL for the service
+
+							        // If you'd like to know more about how to integrate with an OAuth provider, 
+									// I personally like the Instagram API docs: http://instagram.com/developer/authentication/
 						}
 					}
 				}
