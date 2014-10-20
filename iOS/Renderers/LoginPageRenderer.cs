c2194a9923847ed5fa730814a5193feb788a79a9
Jack Sierkstra <jacksierkstra@gmail.com>
2014-10-20 21:52:18 +0200

    Fixed the issue on Xamarin.iOS 8 where the modal view controller couldn't be popped. Fixed it thanks to this post: http://stackoverflow.com/questions/24105390/how-to-login-to-facebook-in-xamarin-forms

--- iOS/Renderers/LoginPageRenderer.cs
+++ iOS/Renderers/LoginPageRenderer.cs
@@ -11,32 +11,44 @@ namespace OAuthTwoDemo.XForms.iOS
 {
 	public class LoginPageRenderer : PageRenderer
 	{
+
+		bool IsShown;
+
 		public override void ViewDidAppear (bool animated)
 		{
 			base.ViewDidAppear (animated);
 
-			var auth = new OAuth2Authenticator (
-				clientId: App.Instance.OAuthSettings.ClientId, // your OAuth2 client id
-				scope: App.Instance.OAuthSettings.Scope, // The scopes for the particular API you're accessing. The format for this will vary by API.
-				authorizeUrl: new Uri (App.Instance.OAuthSettings.AuthorizeUrl), // the auth URL for the service
-				redirectUrl: new Uri (App.Instance.OAuthSettings.RedirectUrl)); // the redirect URL for the service
+			// Fixed the issue that on iOS 8, the modal wouldn't be popped.
+			// url : http://stackoverflow.com/questions/24105390/how-to-login-to-facebook-in-xamarin-forms
+			if(	! IsShown ) {
 
-				
-				
+				IsShown = true;
 
-			auth.Completed += (sender, eventArgs) => {
-				// We presented the UI, so it's up to us to dimiss it on iOS.
-				App.Instance.SuccessfulLoginAction.Invoke();
+				var auth = new OAuth2Authenticator (
+					clientId: App.Instance.OAuthSettings.ClientId, // your OAuth2 client id
+					scope: App.Instance.OAuthSettings.Scope, // The scopes for the particular API you're accessing. The format for this will vary by API.
+					authorizeUrl: new Uri (App.Instance.OAuthSettings.AuthorizeUrl), // the auth URL for the service
+					redirectUrl: new Uri (App.Instance.OAuthSettings.RedirectUrl)); // the redirect URL for the service
 
-				if (eventArgs.IsAuthenticated) {
-					// Use eventArgs.Account to do wonderful things
-					App.Instance.SaveToken(eventArgs.Account.Properties["access_token"]);
-				} else {
-					// The user cancelled
-				}
-			};
 
-			PresentViewController (auth.GetUI (), true, null);
+
+
+				auth.Completed += (sender, eventArgs) => {
+					// We presented the UI, so it's up to us to dimiss it on iOS.
+					App.Instance.SuccessfulLoginAction.Invoke();
+
+					if (eventArgs.IsAuthenticated) {
+						// Use eventArgs.Account to do wonderful things
+						App.Instance.SaveToken(eventArgs.Account.Properties["access_token"]);
+					} else {
+						// The user cancelled
+					}
+				};
+
+				PresentViewController (auth.GetUI (), true, null);
+
+			}
+
 		}
 	}
 }
